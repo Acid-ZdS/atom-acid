91271dbaa12135efb1fde15ccc41dc7d67954364
Bat <gelezbaptiste@openmailbox.org>
2016-06-13 18:50:34 +0100

    premiere version : coloration syntaxique + suggestions basiques

---
+++ README.md
@@ -0,0 +1,5 @@
+# Acid
+
+Acid is a functional language created by the Zeste de Savoir community.
+
+This package provide syntax highlighting for Acid.
---
+++ spec/atom-acid-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAcidView from '../lib/atom-acid-view';
+
+describe('AtomAcidView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
